b9cecccf7af019986f3f79edaf866d89b84428f5
Sean Cross <sean@xobs.io>
2019-08-13 13:56:53 +0800

    disable other architectures
    Python is just too difficult to work with.  Disable cross-compiling.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,7 @@
 BOOST="libboost-dev libboost-filesystem-dev libboost-thread-dev libboost-program-options-dev libboost-python-dev libboost-iostreams-dev libboost-system-dev libboost-chrono-dev libboost-date-time-dev libboost-atomic-dev libboost-regex-dev"
 BOOST_ARM64="libboost-dev:arm64 libboost-filesystem-dev:arm64 libboost-thread-dev:arm64 libboost-program-options-dev:arm64 libboost-python-dev:arm64 libboost-iostreams-dev:arm64 libboost-system-dev:arm64 libboost-chrono-dev:arm64 libboost-date-time-dev:arm64 libboost-atomic-dev:arm64 libboost-regex-dev:arm64"
 BOOST_ARMHF="libboost-dev:armhf libboost-filesystem-dev:armhf libboost-thread-dev:armhf libboost-program-options-dev:armhf libboost-python-dev:armhf libboost-iostreams-dev:armhf libboost-system-dev:armhf libboost-chrono-dev:armhf libboost-date-time-dev:armhf libboost-atomic-dev:armhf libboost-regex-dev:armhf"
+BOOST_I386="libboost-dev:i386 libboost-filesystem-dev:i386 libboost-thread-dev:i386 libboost-program-options-dev:i386 libboost-python-dev:i386 libboost-iostreams-dev:i386 libboost-system-dev:i386 libboost-chrono-dev:i386 libboost-date-time-dev:i386 libboost-atomic-dev:i386 libboost-regex-dev:i386"
 
 if [ $ARCH == "linux_x86_64" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
@@ -20,8 +21,9 @@ fi
 if [ $ARCH == "linux_i686" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3.5-dev qt5-default libqt5opengl5-dev $BOOST \
-                          gcc-5-multilib g++-5-multilib libeigen3-dev qtbase5-dev libpython3.5-dev
+                          xdot pkg-config python3.5-dev \
+                          python3.5-dev:i386 qt5-default:i386 libqt5opengl5-dev:i386 $BOOST_I386 \
+                          gcc-5-multilib g++-5-multilib libeigen3-dev:i386 qtbase5-dev:i386 libpython3.5-dev:i386
   sudo ln -s /usr/include/asm-generic /usr/include/asm
   ln -s /usr/include/x86_64-linux-gnu/zconf.h /usr/include
   sudo apt-get autoremove -y
