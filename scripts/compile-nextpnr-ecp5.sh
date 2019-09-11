b67a567576345458d161e42e7ef97bc22399bcbd
Sean Cross <sean@xobs.io>
2019-09-11 12:13:47 +0800

    ecp5: fix static build on Linux
    Signed-off-by: Sean Cross <sean@xobs.io>

--- scripts/compile-nextpnr-ecp5.sh
+++ scripts/compile-nextpnr-ecp5.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # -- Compile nextpnr-ice40 script
 
 nextpnr_dir=nextpnr
@@ -14,12 +15,12 @@ cd $UPSTREAM_DIR
 
 # -- Clone the sources from github
 test -e $nextpnr_dir || git clone $nextpnr_uri $nextpnr_dir
-git -C $nextpnr_dir pull
+git -C $nextpnr_dir fetch
 git -C $nextpnr_dir checkout $nextpnr_commit
 git -C $nextpnr_dir log -1
 
 test -e $prjtrellis_dir || git clone $prjtrellis_uri $prjtrellis_dir
-git -C $prjtrellis_dir pull
+git -C $prjtrellis_dir fetch
 git -C $prjtrellis_dir checkout $prjtrellis_commit
 git -C $prjtrellis_dir submodule init
 git -C $prjtrellis_dir submodule update
@@ -28,21 +29,27 @@ git -C $prjtrellis_dir log -1
 # -- Copy the upstream sources into the build directory
 mkdir -p $BUILD_DIR/$nextpnr_dir
 mkdir -p $BUILD_DIR/$prjtrellis_dir
-rsync -a $nextpnr_dir $BUILD_DIR/$nextpnr_dir --exclude .git
-rsync -a $prjtrellis_dir $BUILD_DIR/$prjtrellis_dir --exclude .git
+rsync -a $nextpnr_dir $BUILD_DIR --exclude .git
+rsync -a $prjtrellis_dir $BUILD_DIR --exclude .git
 
 cd $BUILD_DIR/
 
 if [ -e $nextpnr_dir/CMakeCache.txt -o -e $prjtrellis_dir/CMakeCache.txt ]
 then
-  echo "CMakeCache.txt exists!"
+    echo "CMakeCache.txt exists!"
 fi
 rm -f $nextpnr_dir/CMakeCache.txt $prjtrellis_dir/CMakeCache.txt
 
 # -- Compile it
-if [ $ARCH == "darwin" ]; then
+if [ $ARCH = "darwin" ]
+then
     cd $BUILD_DIR/$prjtrellis_dir/libtrellis
-    cmake -DBUILD_SHARED=OFF -DSTATIC_BUILD=OFF .
+    cmake \
+        -DBUILD_SHARED=OFF \
+        -DSTATIC_BUILD=ON \
+        -DBUILD_PYTHON=OFF \
+        -DBoost_USE_STATIC_LIBS=ON \
+        .
     make -j$J CXX="$CXX" LIBS="-lm -fno-lto -ldl -lutil"
 
     cd $BUILD_DIR/$nextpnr_dir
@@ -56,27 +63,37 @@ if [ $ARCH == "darwin" ]; then
         -DPYTHON_LIBRARY=/tmp/nextpnr/lib/libpython3.7m.a \
         -DEigen3_DIR=/tmp/nextpnr/share/eigen3/cmake \
         -DBUILD_GUI=OFF \
+        -DBUILD_PYTHON=ON \
         -DBUILD_HEAP=ON \
         -DCMAKE_EXE_LINKER_FLAGS='-fno-lto -ldl -lutil' \
         -DICEBOX_ROOT="$WORK_DIR/icebox" \
         -DSTATIC_BUILD=ON \
         .
     make -j$J CXX="$CXX" LIBS="-lm -fno-lto -ldl -lutil"
-elif [ ${ARCH:0:7} == "windows" ]; then
+elif [ ${ARCH:0:7} = "windows" ]
+then
     echo "Build not functioning on Windows"
     exit 1
 else
     cd $BUILD_DIR/$prjtrellis_dir/libtrellis
-    cmake -DBUILD_SHARED=OFF -DSTATIC_BUILD=OFF .
+    cmake \
+        -DBUILD_SHARED=OFF \
+        -DSTATIC_BUILD=ON \
+        -DBUILD_PYTHON=OFF \
+        -DBoost_USE_STATIC_LIBS=ON \
+        -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" \
+        .
     make -j$J CXX="$CXX"
 
     cd $BUILD_DIR/$nextpnr_dir
     cmake \
+        -DARCH=ecp5 \
         -DEXTERNAL_CHIPDB=$WORK_DIR/ecp5 \
         -DTRELLIS_ROOT=$WORK_DIR/$prjtrellis_dir \
         -DPYTRELLIS_LIBDIR=$WORK_DIR/$prjtrellis_dir/libtrellis \
         -DBUILD_HEAP=ON \
         -DBUILD_GUI=OFF \
+        -DBUILD_PYTHON=ON \
         -DSTATIC_BUILD=ON \
         -DBoost_USE_STATIC_LIBS=ON \
         .
