c30625d911ac32386c4955da4e853067381c6cea
Sean Cross <sean@xobs.io>
2019-09-14 22:26:58 +0800

    darwin: try to remove rpath
    Signed-off-by: Sean Cross <sean@xobs.io>

--- scripts/darwin-patch.sh
+++ scripts/darwin-patch.sh
@@ -7,16 +7,19 @@ then
 fi
 
 echo "Removing @rpath from $prog_name"
+base_pkg=$(echo ${prog_name} | cut -d/ -f5)
+install_name_tool -delete_rpath @rpath/$base_pkg $prog_name || true
 otool -L "$prog_name" | while read i
 do
 	if ! echo $i | grep -q "@rpath"
 	then
-		echo "Skipping library [$i]"
+		echo "    Skipping library [$i]"
 		continue
 	fi
 	base_lib=$(echo "$i" | awk '{print $1}')
 	new_path=$(echo "$base_lib" | sed 's|@rpath|/tmp/nextpnr/lib|')
-	echo "$prog_name: Removing rpath '$base_lib' -> '$new_path'"
+	echo "    $prog_name: Removing rpath '$base_lib' -> '$new_path'"
 	install_name_tool -change "$base_lib" "$new_path" "$prog_name"
 done
 otool -L "$prog_name"
+echo ""
